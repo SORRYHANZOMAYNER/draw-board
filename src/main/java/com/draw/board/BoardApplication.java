fc13dc1bb009bcee28e414c5bf0036a3cce479f6
egor.aksenov <egor.aksenov@ssi-solutions.ru>
2026-08-24 13:28:24 +0400

    fix some mistakes

--- src/main/java/com/draw/board/BoardApplication.java
+++ src/main/java/com/draw/board/BoardApplication.java
@@ -2,12 +2,15 @@ package com.draw.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.draw.board", "components"})
+@EnableJpaRepositories(basePackages = "components.repositories")
+@EntityScan(basePackages = "components.entities")
 public class BoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
-
 }
--- src/main/java/components/entities/Room.java
+++ src/main/java/components/entities/Room.java
@@ -1,6 +1,8 @@
 package components.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Date;
@@ -9,6 +11,7 @@ import java.util.Date;
 public class Room {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     public String name;
     public Date created_at;
